820cb0f161a3300459bf7b11f608cd6d2461b148
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-04-20 20:31:56 +0200

    Create image controller

---
+++ controllers/image.js
@@ -0,0 +1,15 @@
+const handleImagePut = (req, res, postgresDB) => {
+  const { id } = req.body;
+  postgresDB("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0].entries);
+    })
+    .catch((err) => res.status(400).json("Unable to get entries"));
+};
+
+module.exports = {
+  handleImagePut,
+};
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const knex = require("knex");
 const register = require("./controllers/register");
 const signin = require("./controllers/signin");
 const profile = require("./controllers/profile");
-
+const image = require("./controllers/image");
 //Create our app by running express
 const app = express();
 
@@ -52,15 +52,7 @@ app.get("/profile/:id", (req, res) => {
 
 // Incrementing users entries
 app.put("/image", (req, res) => {
-  const { id } = req.body;
-  postgresDB("users")
-    .where("id", "=", id)
-    .increment("entries", 1)
-    .returning("entries")
-    .then((entries) => {
-      res.json(entries[0].entries);
-    })
-    .catch((err) => res.status(400).json("Unable to get entries"));
+  image.handleImagePut(req, res);
 });
 
 //Give our app a port to run in
