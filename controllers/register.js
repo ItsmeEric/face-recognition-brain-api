d390251c939a7cca79c26c2edb14f646a69dce25
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-04-20 20:04:25 +0200

    create controllers
    Created controllers to make the code more appealing and readable. Also added dependence injection for the handleRegister function

---
+++ controllers/register.js
@@ -0,0 +1,41 @@
+const handleRegister = (req, res, postgresDB, bcrypt) => {
+  //Get what we need from the body using destructuring
+  const { email, name, password } = req.body;
+
+  // Hash user password
+  const hash = bcrypt.hashSync(password);
+
+  // Transactions
+  postgresDB
+    .transaction((trx) => {
+      trx
+        .insert({
+          hash: hash,
+          email: email,
+        })
+        .into("login")
+        .returning("email") // Returns added email
+        .then((loginEmail) => {
+          // Insert new user into the DB when registered
+          // And return the info as part of the transaction
+          return trx("users")
+            .returning("*")
+            .insert({
+              name: name,
+              email: loginEmail[0].email,
+              joined: new Date(),
+            })
+            .then((user) => {
+              // Respond with the newly created user
+              res.json(user[0]);
+            });
+        })
+        .them(trx.commit) // Make sure it's committed
+        .then(trx.rollback);
+    })
+    .catch((err) => res.status(400).json("Unable to Register"));
+};
+
+module.exports = {
+  handleRegister: handleRegister,
+};
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require("express");
 const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
 const knex = require("knex");
+
+const register = require("./controllers/register");
+
 //Create our app by running express
 const app = express();
 
@@ -61,41 +64,7 @@ app.post("/signin", (req, res) => {
 
 //Create register to register new users
 app.post("/register", (req, res) => {
-  //Get what we need from the body using destructuring
-  const { email, name, password } = req.body;
-
-  // Hash user password
-  const hash = bcrypt.hashSync(password);
-
-  // Transactions
-  postgresDB
-    .transaction((trx) => {
-      trx
-        .insert({
-          hash: hash,
-          email: email,
-        })
-        .into("login")
-        .returning("email") // Returns added email
-        .then((loginEmail) => {
-          // Insert new user into the DB when registered
-          // And return the info as part of the transaction
-          return trx("users")
-            .returning("*")
-            .insert({
-              name: name,
-              email: loginEmail[0].email,
-              joined: new Date(),
-            })
-            .then((user) => {
-              // Respond with the newly created user
-              res.json(user[0]);
-            });
-        })
-        .them(trx.commit) // Make sure it's committed
-        .then(trx.rollback);
-    })
-    .catch((err) => res.status(400).json("Unable to Register"));
+  register.handleRegister(req, res, postgresDB, bcrypt);
 });
 
 //Getting the user by using id
