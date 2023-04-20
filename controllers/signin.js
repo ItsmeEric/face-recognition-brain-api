2979bafb6ac4adbe61692c89dd12f5116062d435
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-04-21 00:36:09 +0200

    Add validation for Sign In

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,21 +1,24 @@
 const handleSignin = (postgresDB, bcrypt) => (req, res) => {
+  const { email, password } = req.body;
+
+  // Form Validation
+  if (!email || !password) {
+    return res.status(400).json("Incorrect form submission");
+  }
   //Check if entered user exists credentials match the existing ones
   postgresDB
     .select("email", "hash")
     .from("login")
-    .where("email", "=", req.body.email) // Compare emails
+    .where("email", "=", email) // Compare emails
     .then((data) => {
       // compare the hashes
-      const isValidPassword = bcrypt.compareSync(
-        req.body.password,
-        data[0].hash
-      );
+      const isValidPassword = bcrypt.compareSync(password, data[0].hash);
 
       if (isValidPassword) {
         return postgresDB
           .select("*")
           .from("users")
-          .where("email", "=", req.body.email) // Compare user login email with DB existing emails
+          .where("email", "=", email) // Compare user login email with DB existing emails
           .then((user) => {
             res.json(user[0]);
           })
